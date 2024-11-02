65e8f1147df80e378396f57917f8e3f1325ef5a1
rkestt <andrea.fiori.ff@gmail.com>
2024-11-02 23:58:17 +0100

    fine 11 2 2024

--- prova/Scripts/Characters/player.gd
+++ prova/Scripts/Characters/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready():
+	print("Bellaa");
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,10 +18,10 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	move_and_slide();
