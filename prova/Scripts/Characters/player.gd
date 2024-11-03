40818c85840ff410922a51f31fc7b86ab1960d8c
rkestt <andrea.fiori.ff@gmail.com>
2024-11-03 20:56:55 +0100

    non porta le axis

--- prova/Scripts/Characters/player.gd
+++ prova/Scripts/Characters/player.gd
@@ -18,10 +18,11 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	var direction := Input.get_axis("move_left", "move_right") #mhanz
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide();
+#mhanz
