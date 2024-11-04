2a074386b979383e2069d4b1b2be7ae3705b749e
rkestt <andrea.fiori.ff@gmail.com>
2024-11-04 21:26:03 +0100

    Tile, Movement 1.1
    acc/dec

--- prova/Scripts/Characters/player.gd
+++ prova/Scripts/Characters/player.gd
@@ -2,11 +2,21 @@ extends CharacterBody2D
 
 #region Variables
 # * Character proprieters
+	# Animated Sprite
+@onready var animatedSprite = $AnimatedSprite2D
 # * Movement
-@export var moveSpeed : int = 300
+@export var maxMoveSpeed : int = 300
 @export var jumpVelocity = -300.0
-	#gravity
+@export var acceleration : int = 50
+@export var stoppingSpeed : float = 0.2
+	# gravity
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	# movement check
+var isFalling
+var isJumping
+var isJumpCanc
+var isIdling
+var isRunning
 @export var gravityMultiplayer : float = 1.5;
 
 #endregion
@@ -15,25 +25,40 @@ func _physics_process(delta: float) -> void:
 	movement(delta)
 
 func movement(delta) -> void:
-	# * Manage gravity
+	#* Manage gravity
 	if not is_on_floor():
 		velocity.y += (gravity*gravityMultiplayer) * delta
 
-	# * Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	#* Handle jump.
+	isJumping = Input.is_action_just_pressed("jump") and is_on_floor()
+	if isJumping:
 		velocity.y = jumpVelocity
 
-	# * Output Horizontal Movement
+	#* Get Horizontal Movement
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * moveSpeed
-	else:
-		velocity.x = 0
 	
-	move_and_slide()
-	# * Rotate sprite
+	#* Rotate sprite
 	if direction > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif direction < 0:
 		$AnimatedSprite2D.flip_h = true
+	
+	#* Apply animation
+	if velocity.x == 0:
+		animatedSprite.play("idle")
+	elif velocity.x != 0:
+		animatedSprite.play("run")
+	#* Apply movement
+	if direction == 1 or direction == -1:
+		velocity.x += direction * acceleration
+	elif direction == 0:
+		velocity.x = lerp(velocity.x,0.0,stoppingSpeed)
+
+	#Dont let the speed go further
+	if velocity.x > maxMoveSpeed:
+		velocity.x = maxMoveSpeed
+	elif velocity.x < -maxMoveSpeed:
+		velocity.x = -maxMoveSpeed
+	move_and_slide()
+	
 	
